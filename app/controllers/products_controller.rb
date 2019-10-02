0e563e73eaf9333cc50e6916f23a6a220727b085
Adsidera <annamaria.costalonga@gmail.com>
2019-10-02 21:21:00 +0200

    added products controller and spec

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,51 @@
+class ProductsController < ApplicationController
+  before_action :set_product, except: [:index, :new, :create]
+
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+  def new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: { product: @product, message: "Product created" }
+    else
+      render json: { error: @product.errors }
+    end
+  end
+
+  def show
+    render json: @product
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: { product: @product, message: "Product updated"}
+    else
+      render json: { error: @product.errors }
+    end
+  end
+
+  def destroy
+    if @product.destroy
+      render json: { message: "Product deleted" }
+    end
+  end
+
+  private
+  def set_product_user
+    @user ||= User.find(params[:user_id])
+  end
+
+  def set_product
+    @product ||= Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :user_id )
+  end
+end
