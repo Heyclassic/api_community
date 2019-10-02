69cf6c580ffcbb93582b17cd706666552a6858d3
Adsidera <annamaria.costalonga@gmail.com>
2019-10-02 20:33:42 +0200

    added controllers for users and products - controller specs and fabricators

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,44 @@
 class UsersController < ApplicationController
+  before_action :set_user, except: [:index, :new, :create]
 
   def index
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { user: @user, message: "User created" }
+    else
+      render json: { message: @user.errors }
+    end
+  end
+
   def show
+    render json: @user
   end
 
   def update
-  end
-
-  def edit
+    if @user.update(user_params)
+      render json: { user: @user, message: "User updated"}
+    else
+      render json: {message: @user.errors}
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password_digest, :password, :password_confirmation, :name)
+  end
+
+  def set_user
+    @user ||= User.find(params[:id])
   end
 end
