9924d676e5bcdd68ff64a9c3235abadf30c75e23
Adsidera <annamaria.costalonga@gmail.com>
2019-10-10 21:29:41 +0200

    solved activestorage issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
+  include Rails.application.routes.url_helpers
   has_secure_password
 
   has_many :products
   has_one_attached :avatar
 
   def avatar_path
-    ActiveStorage::Blob.service.path_for(avatar.key) if avatar.attached?
+    rails_blob_path(avatar, only_path: true) if avatar.attached?
   end
 end
