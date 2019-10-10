8ef9b5daa0e763e217b16447443348d9d0a3f9f8
Adsidera <annamaria.costalonga@gmail.com>
2019-10-10 23:09:28 +0200

    added maker for included relationship with user

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,6 +1,7 @@
 class ProductSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :description, :images_paths
+
   attribute :maker do |object|
     UserSerializer.new(object.user)
   end
