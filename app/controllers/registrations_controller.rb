0ceb0bdfbb244e98cf2c1f3a2d2ab76f47f3cdda
Adsidera <annamaria.costalonga@gmail.com>
2019-10-24 15:29:51 +0200

    16 - add rubocop config

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Lint/UselessAssignment
 class RegistrationsController < SessionsController
   def create
     command = RegisterUser.call(params[:email], params[:password], params[:password_confirmation]).call
